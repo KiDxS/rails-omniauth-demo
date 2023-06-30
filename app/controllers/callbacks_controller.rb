da0f7c981f2d8997c405b82d31fcd3a18cd08c79
Angelo Geant Gaviola <43493935+KiDxS@users.noreply.github.com>
2023-06-30 09:27:35 +0800

    set the access_token and the other values of the columns

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,13 +1,19 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     auth = request.env['omniauth.auth']
-    @user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
+    service = Service.where(provider: auth.provider, uid: auth.uid).first
+    if service.present?
+      user = service.user
+      OmniAuthService.refresh!(auth)
+    else
+      user = User.create(email: auth.info.email, password: Devise.friendly_token[0, 20])
+      user.services.create(
+        provider: auth.provider, uid: auth.uid, access_token: auth.credentials.token,
+        access_token_secret: auth.credentials.secret,
+        refresh_token: auth.credentials.refresh_token, expires_at: auth.credentials.expires_at
+      )
     end
-    sign_in @user
-    redirect_to "/"
+    sign_in user
+    redirect_to '/'
   end
 end
